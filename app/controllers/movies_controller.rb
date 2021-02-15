3fdd921e5f87216a443cc4b008e1748dae52fd9c
EC2 Default User <ec2-user@ip-172-31-89-192.ec2.internal>
2021-02-15 19:48:07 +0000

    Add part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,7 +34,7 @@ class MoviesController < ApplicationController
     if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:sort].nil? && !session[:sort].nil?)
       redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
       
-    elsif (params[:ratings].nil? && params[:commit]=='Refresh')
+    elsif (params[:ratings].nil? and params[:commit]=='Refresh')
       @ratings_to_show = Movie.all_ratings
       @movies = Movie.with_ratings(@ratings_to_show, session[:sort])
     
